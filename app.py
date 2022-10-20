57c7d8238a3d72b745f6e68cee7ebedf5c517036
Jesse Mammosser <jesse.mamo123@gmail.com>
2022-10-20 13:45:36 -0500

    learning python on  codecademy

--- app.py
+++ app.py
@@ -5,3 +5,8 @@ print("Jesse M")
 print('0---')
 print('||||')
 print("0|||0")
+my_name = "Jesse M"
+print("Hello and welcome " + my_name + "!")
+# comment about my first comment
+first_comment = "1"
+print("hello this is your " + first_comment + " comment")
