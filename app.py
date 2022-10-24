5fa73bfe4f80b86664b80dfa875f4fd1882edb9d
Jesse Mammosser <jesse.mamo123@gmail.com>
2022-10-24 16:33:45 -0500

    feat:python troubleshooting

--- app.py
+++ app.py
@@ -10,3 +10,4 @@ print("Hello and welcome " + my_name + "!")
 # comment about my first comment
 first_comment = "1"
 print("hello this is your " + first_comment + " comment")
+print("hello this is your " + first_comment + " comment")
