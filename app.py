f4f89d005d46a9cdaa38763efd38b0e920fe350d
Jesse Mammosser <jesse.mamo123@gmail.com>
2022-10-25 23:14:58 -0500

    feat:py

--- app.py
+++ app.py
@@ -10,4 +10,3 @@ print("Hello and welcome " + my_name + "!")
 # comment about my first comment
 first_comment = "1"
 print("hello this is your " + first_comment + " comment")
-print("hello this is your " + first_comment + " comment")
