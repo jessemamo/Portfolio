5bbf4581f60ba9e4e35a814bde443298ea4c68c3
Jesse Mammosser <jesse.mamo123@gmail.com>
2022-11-15 12:03:41 -0600

    feat: python timer

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ import time
 def countdown(t):
     while t:
         mins, secs = divmod(t, 60)
-        timer = '{:02d} : {02d}' .format(mins,secs)
+        timer = '{:02d} : {:02d}' .format(mins,secs)
         print(timer, end="\r")
         time.sleep(1)
         t -= 1
 
         print('times up')
 
+t = input('How many seconds do you want?:')
+
+countdown(int(t))
