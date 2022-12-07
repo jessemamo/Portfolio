b87a4b38cd2b10d04f1d67a5bcb43333abac1d9f
Jesse Mammosser <jesse.mamo123@gmail.com>
2022-12-06 22:51:03 -0600

    feat: leetcode

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-import time
-
-def countdown(t):
-    while t:
-        mins, secs = divmod(t, 60)
-        timer = '{:02d} : {:02d}' .format(mins,secs)
-        print(timer, end="\r")
-        time.sleep(1)
-        t -= 1
-
-        print('times up')
-
-t = input('How many seconds do you want?:')
-
-countdown(int(t))
+# import time
+#
+# def countdown(t):
+#     while t:
+#         mins, secs = divmod(t, 60)
+#         timer = '{:02d} : {:02d}' .format(mins,secs)
+#         print(timer, end="\r")
+#         time.sleep(1)
+#         t -= 1
+#
+#         print('times up')
+#
+# t = input('How many seconds do you want?:')
+#
+# countdown(int(t))
 class BinarySearch {
 // Returns index of x if it is present in arr[l..
 // r], else return -1
