925aab80d08578a211b3b420465967c7e863c1ec
Jesse Mammosser <jesse.mamo123@gmail.com>
2022-12-14 14:14:30 -0600

    feat: leetcode

--- main.py
+++ main.py
@@ -84,20 +84,38 @@
 #         """
 #         return sorted(map(lambda x: (x)**2, nums))
 
-class Solution(object):
-    dict = { 1 : 'I',  4 : 'IV', 5 :  'V', 9 : 'IX', 10 : 'X', 40 : 'XL', 50 : 'L',
-             90 : 'XC', 100 : 'C', 400 : 'CD', 500 : 'D', 900 : 'CM', 1000 : 'M'}
-    def intToRoman(self, num):
-        str = ''
-        list = [10, 100, 1000, 10000]   #get the tens/hundreds/thousands place
-        for i in list:
-            if num % i != 0:
-                str = self.compute(num % i, i/10) + str
-                num = num - (num % i)
-        return str
+# class Solution(object):
+#     dict = { 1 : 'I',  4 : 'IV', 5 :  'V', 9 : 'IX', 10 : 'X', 40 : 'XL', 50 : 'L',
+#              90 : 'XC', 100 : 'C', 400 : 'CD', 500 : 'D', 900 : 'CM', 1000 : 'M'}
+#     def intToRoman(self, num):
+#         str = ''
+#         list = [10, 100, 1000, 10000]   #get the tens/hundreds/thousands place
+#         for i in list:
+#             if num % i != 0:
+#                 str = self.compute(num % i, i/10) + str
+#                 num = num - (num % i)
+#         return str
+#
+#     def compute(self, num, lsb):
+#         if num in self.dict:
+#             return self.dict[num]
+#         else:
+#             return self.compute(num-lsb, lsb) + self.dict[lsb]
 
-    def compute(self, num, lsb):
-        if num in self.dict:
-            return self.dict[num]
-        else:
-            return self.compute(num-lsb, lsb) + self.dict[lsb]
+class Solution(object):
+    def romanToInt(self, s):
+        const_dct = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+
+        lst = [const_dct[i] for i in s]
+        res = 0
+        idx = 0
+
+        while idx < len(lst):
+            if idx < len(lst) - 1 and lst[idx] < lst[idx + 1]:
+                res -= lst[idx]
+                idx += 1
+            else:
+                res += lst[idx]
+                idx += 1
+
+        return res
