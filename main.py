0f01914b0f3c52ead665a4bc71cb80e1f1bf1a53
Jesse Mammosser <jesse.mamo123@gmail.com>
2023-01-11 09:45:25 -0600

    feat: leetcode

--- main.py
+++ main.py
@@ -196,43 +196,50 @@
 # first.next=second
 # return head
 
+# class Solution(object):
+#     def reverseKGroup(self, head, k):
+#         new = ListNode()
+#         new.next = head
+#
+#         curr = head
+#         prev = new
+#         last_end = new
+#         is_valid = True
+#
+#         while curr:
+#
+#             counter = 1
+#             checker = curr
+#
+#             while counter < k:
+#                 checker = checker.next
+#                 if checker is None:
+#                     is_valid = False
+#                     break
+#                 counter += 1
+#
+#             if not is_valid:
+#                 break
+#
+#             counter = 0
+#
+#             while counter < k:
+#                 temp = curr.next
+#                 curr.next = prev
+#                 prev = curr
+#                 curr = temp
+#                 counter += 1
+#
+#             swap_end = last_end.next
+#             last_end.next = prev
+#             swap_end.next = curr
+#             last_end = swap_end
+#
+#         return new.next
+
 class Solution(object):
-    def reverseKGroup(self, head, k):
-        new = ListNode()
-        new.next = head
-
-        curr = head
-        prev = new
-        last_end = new
-        is_valid = True
-
-        while curr:
-
-            counter = 1
-            checker = curr
-
-            while counter < k:
-                checker = checker.next
-                if checker is None:
-                    is_valid = False
-                    break
-                counter += 1
-
-            if not is_valid:
-                break
-
-            counter = 0
-
-            while counter < k:
-                temp = curr.next
-                curr.next = prev
-                prev = curr
-                curr = temp
-                counter += 1
-
-            swap_end = last_end.next
-            last_end.next = prev
-            swap_end.next = curr
-            last_end = swap_end
-
-        return new.next
+    def removeDuplicates(self, nums):
+        for i in nums:
+            while i in nums[nums.index(i)+1:]:
+                nums.remove(i)
+        return len(nums)
