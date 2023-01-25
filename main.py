4e67af473bceb968aece2886ec2edc0d72d0149f
Jesse Mammosser <jesse.mamo123@gmail.com>
2023-01-25 12:29:32 -0600

    feat: leetcode

--- main.py
+++ main.py
@@ -397,27 +397,81 @@
 #         except:
 #             return r
 #         return r
+#
+# class Solution(object):
+#     def isValidSudoku(self, board):
+#         """
+#         :type board: List[List[str]]
+#         :rtype: bool
+#         """
+#         cols = collections.defaultdict(set)
+#         rows = collections.defaultdict(set)
+#         squares = collections.defaultdict(set)
+#
+#         for r in range(9):
+#             for c in range (9):
+#                 if board[r][c] == ".": #if empty
+#                     continue
+#                 if (board[r][c] in rows[r] or
+#                         board[r][c] in cols[c] or
+#                         board[r][c] in squares[(r//3,c//3)]):
+#                     return False
+#                     #update the sets
+#                 cols[c].add(board[r][c])
+#                 rows[r].add(board[r][c])
+#                 squares[(r//3, c//3)].add(board[r][c])
+#         return True
 
 class Solution(object):
-    def isValidSudoku(self, board):
+    def solveSudoku(self, board):
         """
         :type board: List[List[str]]
-        :rtype: bool
+        :rtype: None Do not return anything, modify board in-place instead.
         """
-        cols = collections.defaultdict(set)
-        rows = collections.defaultdict(set)
-        squares = collections.defaultdict(set)
 
-        for r in range(9):
-            for c in range (9):
-                if board[r][c] == ".": #if empty
-                    continue
-                if (board[r][c] in rows[r] or
-                        board[r][c] in cols[c] or
-                        board[r][c] in squares[(r//3,c//3)]):
-                    return False
-                    #update the sets
-                cols[c].add(board[r][c])
-                rows[r].add(board[r][c])
-                squares[(r//3, c//3)].add(board[r][c])
+        solve_sudoku(board)
+
+def solve_sudoku(grid):
+    row, col = find_empty_cell(grid)
+    if row == -1:
         return True
+
+    for num in range(1, 10):
+        if is_safe(grid, row, col, num):
+            grid[row][col] = str(num)
+
+            if solve_sudoku(grid):
+                return True
+
+            grid[row][col] = '.'
+
+    return False
+
+def find_empty_cell(grid):
+    for i in range(9):
+        for j in range(9):
+            if grid[i][j] == '.':
+                return (i, j)
+
+    return (-1, -1)
+
+def is_safe(grid, row, col, num):
+    # Check if the number is already in the current row
+    for i in range(9):
+        if grid[row][i] == str(num):
+            return False
+
+    # Check if the number is already in the current column
+    for i in range(9):
+        if grid[i][col] == str(num):
+            return False
+
+    # Check if the number is already in the current 3x3 box
+    start_row = row - row % 3
+    start_col = col - col % 3
+    for i in range(3):
+        for j in range(3):
+            if grid[start_row + i][start_col + j] == str(num):
+                return False
+
+    return True
